8b8b34dbff369d4726cdb68463263a08784e0fa3
Jonathan Henderson <jkhenderson999@gmail.com>
2021-02-24 14:45:17 -0600

    Update User

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,29 +1,27 @@
-import { Schema, Document, ObjectId, model} from 'mongoose';
+import { Schema, Document, model} from 'mongoose';
 
-import { BadgeInterface, BadgeSchema } from './badge';
-import { NotificationInterface } from './notification';
 import { logger } from '../utils/logger';
 
 export const name = 'User';
 
 // Create an interface for our User model
-// Notifications are an array of Mongo IDs
-// Badges are an array of Badge objects
 export interface UserInterface extends Document {
   username: string,
   full_name: string,
-  email: string,
+  email: string, 
   password: string,
-  salt: string,
-  hash_iterations: number,
-  created_at: Date,
-  updated_at: Date,
-  is_admin: boolean,
-  current_team: string,
-  notifications: [NotificationInterface['_id']]
-  badges: [ObjectId]
+  salt: string, // The random bytes added to their password before hashing
+  hash_iterations: number, // The number of times their password is hashed
+  created_at: Date, // When this user was created
+  updated_at: Date, // The last time this user was updated
+  is_admin: boolean, // Whether this is an administrator (on a need basis)
+  mmai_team: string, // This user's competition team, if any
+  club_team: string, // This user's main club team, if any (internal developers)
+  notifications: [string], // An array of notification IDs
+  badges: [string] // An array of badge IDs
 };
 
+// Set up the database schema for a User
 const UserSchema = new Schema({
   username: {
     type: String,
@@ -63,14 +61,22 @@ const UserSchema = new Schema({
     type: Boolean,
     required: true
   },
-  current_team: {
+  mmai_team: {
     type: String,
     required: false
   },
-  // badges: {
-  //   type: [BadgeSchema],
-  //   required: true
-  // }
+  club_team: {
+    type: String,
+    required: false
+  },
+  notifications: {
+    type: [String],
+    default: []
+  },
+  badges: {
+    type: [String],
+    default: []
+  }
 });
 
 UserSchema.set('toJSON', {
