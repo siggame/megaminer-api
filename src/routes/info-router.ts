96f46211c52753ff85e403728411e5af4a5d4f63
Jonathan Henderson <jkhenderson999@gmail.com>
2021-02-25 13:58:44 -0600

    Add info router, remote test

---
+++ src/routes/info-router.ts
@@ -0,0 +1,38 @@
+import { Router, Request, Response, NextFunction } from "express";
+import { RouteInfo, swaggerSetup } from "../services/swaggerService";
+import { projectPackage } from "../utils/package";
+
+/**
+ * Returns information about this application.
+ */
+async function getInfo(req: Request, res: Response, next: NextFunction) {
+  try {
+    const info = {
+      name: projectPackage.name,
+      description: projectPackage.description || "",
+      version: projectPackage.version,
+      homepage: projectPackage.homepage,
+    };
+
+    return res.status(200).json(info);
+  } catch (err) {
+    return next(err);
+  }
+}
+
+const router = Router();
+
+router.get("/", getInfo);
+
+export { router };
+
+const infoRouteInfo: RouteInfo = {
+  "x-swagger-router-controller": "TestRoute",
+  operationId: "test",
+  tags: ["Info"],
+  description: "Get application information.",
+  parameters: [],
+  responses: {},
+};
+
+swaggerSetup.addRouteToSwaggerDoc("/info", "get", infoRouteInfo);
