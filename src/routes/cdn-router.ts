28723c8a64a372a7837bf40a24bf2349b2b3035b
Jonathan Henderson <jkhenderson999@gmail.com>
2021-04-06 18:15:19 -0500

    Add wrench route

--- src/routes/cdn-router.ts
+++ src/routes/cdn-router.ts
@@ -1,22 +1,35 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { RouteInfo, swaggerSetup } from "../services/swaggerService";
 
+const staticFileRoot = { root: `${__dirname}/../../static` };
+
+const router = Router();
+
 /**
  * Returns main CSS file.
  */
 async function getCSS(req: Request, res: Response, next: NextFunction) {
   try {
-    return res
-      .status(200)
-      .sendFile("main.css", { root: `${__dirname}/../../static` });
+    return res.status(200).sendFile("main.css", staticFileRoot);
   } catch (err) {
     return next(err);
   }
 }
 
-const router = Router();
-
 router.get("/css", getCSS);
+
+/**
+ * Returns the ACM Game wrench logo.
+ */
+async function getWrench(req: Request, res: Response, next: NextFunction) {
+  try {
+    return res.status(200).sendFile("wrench.svg", staticFileRoot);
+  } catch (err) {
+    return next(err);
+  }
+}
+
+router.get("/wrench", getWrench);
 
 export { router };
 
@@ -30,3 +43,14 @@ const cssRouteInfo: RouteInfo = {
 };
 
 swaggerSetup.addRouteToSwaggerDoc("/cdn/css", "get", cssRouteInfo);
+
+const wrenchRouteInfo: RouteInfo = {
+  "x-swagger-router-controller": "cdnRouter",
+  operationId: "wrench",
+  tags: ["CDN"],
+  description: "Get the ACM Game wrench logo.",
+  parameters: [],
+  responses: {},
+};
+
+swaggerSetup.addRouteToSwaggerDoc("/cdn/wrench", "get", wrenchRouteInfo);
