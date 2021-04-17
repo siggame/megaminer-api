160513f163a3275cf927e3c9febbf1b2efb09b07
Jonathan Henderson <jkhenderson999@gmail.com>
2021-04-16 19:59:14 -0500

    Add name

--- startDocker.sh
+++ startDocker.sh
@@ -3,6 +3,7 @@
 docker build . -t mmai-api
 
 docker run -it \
+--name="mmai-api" \
 --network="mmai-backend-network" \
 --mount type=bind,source="$(pwd)"/configs,target=/usr/app/configs \
 -p 127.0.0.1:3000:3000 mmai-api
