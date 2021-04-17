a1381bee4e0178e6daa627bd205cbe371b3bfeca
Jonathan Henderson <jkhenderson999@gmail.com>
2021-04-17 17:22:53 -0500

    Remove docker scripts

--- startDocker.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-docker build . -t mmai-api
-
-docker run -it \
---name="mmai-api-container" \
---network="mmai-backend-network" \
---mount type=bind,source="$(pwd)"/configs,target=/usr/app/configs \
--p 127.0.0.1:3000:3000 mmai-api
