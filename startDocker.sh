dc89f9e27eceb7a6291140768b9b0dde404c9442
Jonathan Henderson <jkhenderson999@gmail.com>
2021-04-16 20:15:29 -0500

    Rename

--- startDocker.sh
+++ startDocker.sh
@@ -3,7 +3,7 @@
 docker build . -t mmai-api
 
 docker run -it \
---name="mmai-api" \
+--name="mmai-api-container" \
 --network="mmai-backend-network" \
 --mount type=bind,source="$(pwd)"/configs,target=/usr/app/configs \
 -p 127.0.0.1:3000:3000 mmai-api
